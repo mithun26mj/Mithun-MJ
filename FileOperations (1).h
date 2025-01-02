975df5a5fc178c8ffc1742397282680bc75dd437
mithun26mj <mithunmj0022@gmail.com>
2025-01-02 16:05:28 +0530

    Update FileOperations (1).h

--- FileOperations (1).h
+++ FileOperations (1).h
@@ -1,156 +1,106 @@
-#ifndef FILEOPERATIONS_H
-#define FILEOPERATIONS_H
+import java.io.*;
+import java.util.*;
 
-#include <cstdlib>
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <stack>
+public class FileOperations {
 
-#include "Location.h"
+    private static final float MULTI = 3;
 
-using namespace std;
+    // Parse locations and routes from file
+    public static List<Location> locationParser(String filename, List<Route> routes) {
+        List<Location> cities = new ArrayList<>();
+        try (BufferedReader locations = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = locations.readLine()) != null) {
+                String[] parts = line.split(",");
+                String country = parts[0];
+                String city = parts[1];
+                float latitude = Float.parseFloat(parts[2]);
+                float longitude = Float.parseFloat(parts[3]);
 
-vector<Location*> locationParser(string filename, vector<Route*> routes){
-	fstream locations(filename.c_str());
+                Location node = new Location(country, city, latitude, longitude);
+                for (Route route : routes) {
+                    if (route.getOriginS().equals(node.getCapital())) {
+                        route.setOrigin(node);
+                        node.getRoutes().add(route);
+                    } else if (route.getDestinationS().equals(node.getCapital())) {
+                        route.setDestination(node);
+                    }
+                }
 
-	string country;
-	string city;
-	string latitude;
-	string longitude;
+                cities.add(node);
+            }
+            System.out.println("Cities Parsed from: " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
 
-	vector<Location*> cities;
-	Location* node;
+    // Parse routes from a CSV file
+    public static List<Route> routeParser(String filename) {
+        List<Route> allRoutes = new ArrayList<>();
+        try (BufferedReader routes = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = routes.readLine()) != null) {
+                String[] parts = line.split(",");
+                String originS = parts[0];
+                String destinationS = parts[1];
+                String transport = parts[2];
+                float time = Float.parseFloat(parts[3]);
+                float cost = Float.parseFloat(parts[4]);
+                String note = parts[5];
 
-	while(locations.good()){
-		getline(locations, country, ',');
-		getline(locations, city, ',');
-		getline(locations, latitude, ',');
-		getline(locations, longitude);
-		//cout << "Country:" << country << " City:" << city << " Lat:" << latitude << " Lon:" << longitude << endl << endl << endl;
+                Location origin = new Location();
+                Location destination = new Location();
+                Route route = new Route(origin, destination, transport, time, cost, note);
+                route.setOriginS(originS);
+                route.setDestinationS(destinationS);
 
-		node = new Location(country, city, atof(latitude.c_str()), atof(longitude.c_str()));
+                allRoutes.add(route);
+            }
+            System.out.println("Routes Parsed from: " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allRoutes;
+    }
 
-		vector<Route*>::iterator it = routes.begin();
+    // Generate an HTML file displaying routes on a map
+    public static void outputGenerator(String filename, Stack<Location> cities, Stack<Route> routes, boolean costOrTime) {
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(filename))) {
+            output.write("<HTML><HEAD><TITLE>Shortest path from Italy to Kazakhstan</TITLE></HEAD>");
+            output.write("<script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>");
+            output.write("<script>function initialize() { var myOptions = { zoom: 3, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById('map'), myOptions);\n");
 
-		while(it != routes.end()){
-			if((*it) -> originS.compare(node -> capital) == 0){
-				(*it) -> origin = node;
-				node -> routes.push_back((*it));
-			}
-			else if((*it) -> destinationS.compare(node -> capital) == 0){
-				(*it) -> destination = node;
-			}
-			++it;
-		}
+            int markerCount = 0;
+            int contentStringCount = 0;
+            while (!cities.isEmpty() && !routes.isEmpty()) {
+                Location origin = cities.pop();
+                Location destination = cities.pop();
+                Route route = routes.pop();
 
-		cities.push_back(node);
+                output.write("var marker" + markerCount + " = new google.maps.Marker({ position: new google.maps.LatLng(" + origin.getLat() + ", " + origin.getLon() + "), map: map, title: \"" + origin.getCapital() + ", " + origin.getCountry() + "\"});\n");
+                markerCount++;
+                output.write("var marker" + markerCount + " = new google.maps.Marker({ position: new google.maps.LatLng(" + destination.getLat() + ", " + destination.getLon() + "), map: map, title: \"" + destination.getCapital() + ", " + destination.getCountry() + "\"});\n");
+                markerCount++;
 
-	}
-	cout << "Cities Parsed from: " << filename << endl;
-	return cities;
+                float cost = route.getCost();
+                if (route.getTransport().equals("plane")) {
+                    cost = cost / MULTI;
+                }
+
+                output.write("var contentString" + contentStringCount + " = \"" + origin.getCapital() + ", " + origin.getCountry() + " --> " + destination.getCapital() + ", " + destination.getCountry() + "(" + route.getTransport() + " - " + route.getTime() + " hours - $" + cost + ")\";");
+                output.write("var path" + contentStringCount + " = new google.maps.Polyline({ path: [new google.maps.LatLng(" + origin.getLat() + ", " + origin.getLon() + "), new google.maps.LatLng(" + destination.getLat() + ", " + destination.getLon() + ")], strokeColor: '#0000FF', strokeOpacity: 1.0, strokeWeight: 2});");
+                output.write("path" + contentStringCount + ".setMap(map); google.maps.event.addListener(path" + contentStringCount + ", 'click', function(event) { alert(contentString" + contentStringCount + ");});\n");
+
+                contentStringCount++;
+            }
+
+            output.write("} google.maps.event.addDomListener(window, 'load', initialize); </script></HEAD><BODY><div id='map' style='width:100%;height:100%;'></div></BODY></HTML>");
+            System.out.println("Output File Generated: " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
-
-/*
-Parses and builds Route objects from file routes.csv
-*/
-
-vector<Route*> routeParser(string filename){
-
-	fstream routes(filename.c_str());
-
-	string originS;
-	string destinationS;
-
-	Location* origin = new Location();
-	Location* destination = new Location();
-
-	string type;
-	string time;
-	string cost;
-	string note;
-
-	vector<Route*> allRoutes;
-	Route* edge;
-
-	while(routes.good()){
-		getline(routes, originS, ',');
-		getline(routes, destinationS, ',');
-		getline(routes, type, ',');
-		getline(routes, time, ',');
-		getline(routes, cost, ',');
-		getline(routes, note);
-
-		//cout << "Origin: " << originS << " Destination: " << destinationS << "---" << type << " " << time << " " << cost << " " << endl; //debug
-
-		edge = new Route(origin, destination, type, atof(time.c_str()), atof(cost.c_str()), note);
-		edge -> destinationS = destinationS;
-		edge -> originS = originS;
-
-		allRoutes.push_back(edge);
-	}
-
-	cout << "Routes Parsed from: " << filename << endl;
-	return allRoutes;
-}
-
-
-
-/*
-Generates an appropriately formatted html file that displays the route information passed in by the stack fo cities and routes
-*/
-void outputGenerator(string filename, stack<Location*> cities, stack<Route*> routes, bool costOrTime){
-	
-	ofstream output(filename.c_str());
-	output << "<HTML><HEAD><TITLE>Shortest path from Italy to Kazakhstan</TITLE></HEAD><script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script><script>function initialize() { var myOptions = { zoom: 3, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById('map'), myOptions);\n";
-
-	int markerCount = 0;
-	int contentStringCount = 0;
-	Location* origin;
-	Location* destination;
-	Route* route;
-
-	float cost;
-
-	
-
-	while(!cities.empty() && !routes.empty()){
-
-		//cout << routes.size() << endl << cities.size() << "--" << endl;		//debug
-
-		origin = cities.top();
-		cities.pop();
-		destination = cities.top();
-						
-		output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << origin -> lat << ", " << origin -> lon << "), map: map, title: \"" << origin -> capital << ", " << origin -> country << "\"});\n";
-		
-		markerCount++;
-			
-		output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << destination -> lat << ", " << destination -> lon << "), map: map, title: \"" << destination -> capital << ", " << destination -> country << "\"});\n";
-		
-		markerCount++;
-
-		route = routes.top();
-		routes.pop();
-
-		cost = route -> cost;
-		if(route -> transport.compare("plane") == 0){
-			cost =  cost / MULTI;
-		}
-
-		output << "var contentString" << contentStringCount << " = \"" << origin -> capital << ", " << origin -> country << " --> " << destination -> capital << ", " << destination -> country << "(" << route -> transport << " - " << route -> time << " hours - $" << cost << ")\"; var path" << contentStringCount << " = new google.maps.Polyline({ path: [new google.maps.LatLng(" << origin -> lat << ", " << origin -> lon << "), new google.maps.LatLng(" << destination -> lat << ", " << destination -> lon << ")], strokeColor: '#0000FF', strokeOpacity: 1.0, strokeWeight: 2}); path"<< contentStringCount <<".setMap(map); google.maps.event.addListener(path" << contentStringCount << ", 'click', function(event) { alert(contentString" << contentStringCount << ");});\n";
-
-		contentStringCount++;
-		
-	}
-
-	output << "} google.maps.event.addDomListener(window, 'load', initialize); </script></HEAD><BODY><div id='map' style='width:100%;height:100%;'></div></BODY></HTML>";
-	output.close();
-
-	cout << "Output File Generated: " << filename << endl;
-}
-
-
-#endif
